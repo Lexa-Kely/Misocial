9153b327ebc21157b933f5ff03d5f1b7f595b3dd
Lexa_Kelly <koronaoleksy2010@gmail.com>
2024-11-27 23:48:59 +0200

    ffw

---
+++ docs/js/scripts.js
@@ -0,0 +1,2 @@
+burger.onclick=function(){burger.classList.toggle("active_burger")},closeButton.onclick=function(){burger.classList.remove("active_burger")};
+"use strict";burger.onclick=function(){burger.classList.toggle("active_burger")},closeButton.onclick=function(){burger.classList.remove("active_burger")};
